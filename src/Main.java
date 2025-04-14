2929e868fb94c9c359c32c793fd09b68a5e0b4a7
Elian <Elian.E.K@hotmail.com>
2025-04-14 14:49:10 -0300

    feature: commit 1(sin cambios)

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 15; i++) {
 
-            System.out.println("i = " + i);
+            System.out.println("i es igual a  = " + i);
         }
     }
 }
